ff3a0897454637f59e629927f9b3f7aa93154903
matthew murphy <mattyhead@gmail.com>
2016-08-20 06:27:13 -0400

    Need to explicitly import pagination

--- admin/models/items.php
+++ admin/models/items.php
@@ -100,7 +100,7 @@ class PvnewModelItems extends JModel
     {
         // Load the content if it doesn't already exist
         if (empty($this->_pagination)) {
-//            jimport('joomla.html.pagination');
+            jimport('joomla.html.pagination');
             $this->_pagination = new JPagination($this->getTotal(), $this->getState('limitstart'), $this->getState('limit'));
         }
 
