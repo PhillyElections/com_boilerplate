e9dfd48a0dd04652a37139f401b6f80de90e5787
matthew murphy <mattyhead@gmail.com>
2016-08-19 20:19:15 -0400

    adding some missing indexes

---
+++ site/views/models/items/view.html.php
@@ -0,0 +1,35 @@
+<?php
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Items View for Pvnew Component
+ *
+ * @package    Philadelphia.Votes
+ * @subpackage Components
+ * @license        GNU/GPL
+ */
+class PvnewViewItems extends JView
+{
+    /**
+     * Pvnew view display method
+     * @return void
+     **/
+    public function display($tpl = null)
+    {
+        JToolBarHelper::title(JText::_('PVNews Items Manager'), 'generic.png');
+        JToolBarHelper::deleteList();
+        JToolBarHelper::editListX();
+        JToolBarHelper::addNewX();
+
+        $t = &JToolbar::getInstance('toolbar');
+
+        $items      = &$this->get('Data');
+        $pagination = &$this->get('Pagination');
+
+        $this->assignRef('items', $items);
+        $this->assignRef('pagination', $pagination);
+
+        parent::display($tpl);
+    }
+}
