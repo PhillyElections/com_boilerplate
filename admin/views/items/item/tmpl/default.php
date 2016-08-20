0a8db03e7c6dd559cb000f3078fe796ad5c9a280
matthew murphy <mattyhead@gmail.com>
2016-08-19 20:28:00 -0400

    adding missing indexes

---
+++ admin/views/items/item/tmpl/default.php
@@ -0,0 +1,33 @@
+<?php
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+$item = $this->item;
+
+?>
+<form action="<?=JRoute::_('index.php?option=com_pvnew');?>" method="post" id="adminForm" name="adminForm" class="form-validate">
+    <table cellpadding="0" cellspacing="0" border="0" class="adminform">
+        <tbody>
+            <tr>
+                <td width="200" height="30">
+                    <label id="namemsg" for="field">
+                        <?=JText::_('FIELD');?>:
+                    </label>
+                </td>
+                <td>
+                    <input type="text" id="field" name="field" size="62" value="<?=$item->field;?>" class="input_box required" maxlength="60" placeholder="<?=JText::_('FIELD PLACEHOLDER');?>" />
+                </td>
+            </tr>
+            <tr>
+                <td height="30">&nbsp;</td>
+                <td>
+                    <button class="button validate" type="submit"><?=$this->isNew ? JText::_('SUBMIT') : JText::_('UPDATE');?></button>
+                    <input type="hidden" name="task" value="<?=$this->isNew ? 'add' : 'update';?>" />
+                    <input type="hidden" name="controller" value="item" />
+                    <input type="hidden" name="id" value="<?=$item->id;?>" />
+                    <?=JHTML::_('form.token');?>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</form>
